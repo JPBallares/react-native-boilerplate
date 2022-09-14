2792a939e3bde89d8e9378759d16b75ed982c11f
Justine Ballares <ballaresjustine@gmail.com>
2022-09-15 02:32:15 +0800

    Fix old drawer config

--- src/navigators/NavigationRouter.tsx
+++ src/navigators/NavigationRouter.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import Icon from 'react-native-vector-icons/Feather';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import Home from '@/src/screens/Home';
 import Login from '@/src/screens/Login';
@@ -10,34 +10,19 @@ import Schools from '@/src/screens/Schools';
 
 // import {BackButton} from './NavItems';
 // import styles from './styles';
-// import SideBar from './SideBar';
+import SideBar from './SideBar';
 import screenNames from './screenNames';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const NavigationRouter = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={screenNames.Home} component={Home} />
-        <Stack.Screen name={screenNames.Login} component={Login} />
-        <Stack.Screen name={screenNames.Schools} component={Schools} />
-      </Stack.Navigator>
-      {/* <Scene key="root" hideNavBar>
-        <Drawer
-          key="drawer"
-          drawerIcon={<Icon name="menu" style={styles.navIcon} />}
-          contentComponent={SideBar}>
-          <Scene key="home" title="Home" component={Home} initial />
-          <Scene key="schools" title="Schools" component={Schools} />
-        </Drawer>
-        <Scene
-          key="login"
-          title="Login"
-          component={Login}
-          renderLeftButton={BackButton}
-        />
-      </Scene> */}
+      <Drawer.Navigator drawerContent={() => <SideBar />}>
+        <Drawer.Screen name={screenNames.Home} component={Home} />
+        <Drawer.Screen name={screenNames.Login} component={Login} />
+        <Drawer.Screen name={screenNames.Schools} component={Schools} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
